d796c92420b9e0c0f168664cfc0b3d13d1949699
Tsypa90 <URSA2004@mail.ru>
2022-08-27 00:15:05 +0300

    1. Spring boot repository [#2095 #336814]

--- src/main/java/ru/job4j/forum/service/UserService.java
+++ src/main/java/ru/job4j/forum/service/UserService.java
@@ -1,8 +1,20 @@
 package ru.job4j.forum.service;
 
 import org.springframework.stereotype.Service;
+import ru.job4j.forum.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class UserService {
+ private final Map<Integer, User> users = new HashMap<>();
+ private final AtomicInteger ids = new AtomicInteger(1);
+
+ public User save(User user) {
+  user.setId(ids.getAndIncrement());
+  return users.put(user.getId(), user);
+ }
 }
 
